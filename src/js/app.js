53e2c92b11e9f7dfcd7c938ed79e470040fe05a6
aleynayucesoy <aleyyucesoy@gmail.com>
2022-06-22 11:38:22 +0300

    header update

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,64 @@
-window.$ = window.jQuery = require('jquery');    
+window.$ = window.jQuery = require("jquery");
 require("./owl.carousel.min");
 
-console.log('hello world!');
+console.log("hello world!");
 
-$('.hero-slider').owlCarousel({
-    items: 1,
-    loop:true,
-    margin:1,
-    nav:false,
-    dots: false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    animateOut: 'fadeOut'  
-})
+$(".hero-slider").owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 1,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  animateOut: "fadeOut",
+});
 
-$(".open-menu").on("click", ()=> {
-    $(".sidebar-menu").toggleClass("open")
-    $(".menu .navbar").toggleClass("d-none");
-})
+var scrollLink = $(".scroll");
+
+scrollLink.on("click", function (e) {
+  e.preventDefault();
+
+  $("body,html")
+    .stop(true, true)
+    .animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      1000,
+      "easeOutExpo"
+    );
+});
+
+/*
+ * Left menu active switch on scroll to section
+ */
+$(window).on("scroll", function () {
+  var scrollbarLocation = $(this).scrollTop();
+  scrollLink.each(function () {
+    var sectionOffset = $(this.hash).offset().top - 20;
+    if (sectionOffset <= scrollbarLocation) {
+      $(this).parent().addClass("active");
+      $(this).parent().siblings().removeClass("active");
+    }
+  });
+});
+
+/*
+ * Responsive nav menu
+ */
+$(".desktop-menu-btn").on("click", function (e) {
+  e.preventDefault();
+  $("body").toggleClass("dmenu-close");
+});
+
+$(".mobile-menu-btn").on("click", function (e) {
+  e.preventDefault();
+  $(".sidebar-menu").toggleClass("active");
+});
+
+$(".tabmenu-mobile").on("click", function (e) {
+  e.preventDefault();
+  $(".toolbar").toggleClass("active");
+});
